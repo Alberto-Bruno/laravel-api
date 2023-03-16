38877538d41d0cf959ffe2e5df2e4d483aa1fd4a
Alberto-Bruno <brunoalberto97@outlook.it>
2023-03-16 17:05:10 +0100

    add api, projectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -14,7 +14,9 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::orderBy('updated_at', 'DESC')->with('technologies', 'type')->get();
-
+        foreach ($projects as $project) {
+            if ($project->image) $project->image = url('storage/' . $project->image);
+        }
         return response()->json($projects);
     }
 
